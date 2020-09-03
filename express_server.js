132494730649f82d2e872e5b93d25626cd19dacf
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-03 01:04:00 +0000

    Change back the original password of one of the test users

--- express_server.js
+++ express_server.js
@@ -17,8 +17,7 @@ const users = {
   "aJ48lW": {
     id: "aJ48lW",
     email: "user@example.com",
-    password: "123"
-    // password: "purple-monkey-dinosaur"
+    password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
     id: "user2RandomID",
