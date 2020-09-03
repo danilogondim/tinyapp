5fbbedef751d1a523d4d79cc7e0c55fdc9636759
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-03 23:27:17 +0000

    Review and clean comments

--- express_server.js
+++ express_server.js
@@ -5,8 +5,10 @@ const bcrypt = require('bcrypt');
 const { urlDatabase, users } = require('./databases/db');
 const { getUserByEmail, userURLs, generateRandomString } = require('./helpers');
 
+
 const app = express();
 const PORT = 8080;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
@@ -14,8 +16,10 @@ app.use(cookieSession({
   keys: ['aRandomWritTen_key with non-sense charactersajsalkasaolska', '1abo9031,daxj']
 }));
 
+
 app.set("view engine", "ejs");
 
+// redirects to the urls list if the user is already logged in. If not, it will redirect to the login page
 app.get('/', (req, res) => {
   if (req.session.user_id) {
     return res.redirect('urls');
@@ -23,7 +27,7 @@ app.get('/', (req, res) => {
   res.redirect('login');
 });
 
-
+// renders the urls page
 app.get("/urls", (req, res) => {
   const user = users[req.session.user_id];
   const urls = userURLs(user, urlDatabase);
@@ -34,8 +38,8 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// if an user is logged in, it renders the form to create a new short URL. Otherwise, it redirects to the login page
 app.get("/urls/new", (req, res) => {
-  // only registered users can shorten URLs. If the user is not logged in, the page redirects to the login page
   if (req.session.user_id) {
     const templateVars = {
       user: users[req.session.user_id]
@@ -45,6 +49,26 @@ app.get("/urls/new", (req, res) => {
   res.redirect('/login');
 });
 
+// creates new shortened urls if the user is logged in
+app.post("/urls", (req, res) => {
+  const userID = req.session.user_id;
+  if (!userID) {
+    return res.status(400).send("<h1>400 Bad Request</h1><p>You need to be logged in in order to short an URL.</p>");
+  }
+  const shortURL = generateRandomString();
+  let longURl = req.body.longURL;
+  // in case the user did not pass the url with a protocol, it will add the http protocol
+  if (!(longURl.match(/^(https:\/\/|http:\/\/)/))) {
+    longURl = `http://www.${longURl}`;
+  }
+  urlDatabase[shortURL] = {
+    "longURL": longURl,
+    userID
+  };
+  return res.redirect(`/urls/${shortURL}`);
+});
+
+// gather the necessary data and renders the urls_show template accordingly to the user and database information
 app.get("/urls/:shortURL", (req, res) => {
   const user = users[req.session.user_id];
   const urls = userURLs(user, urlDatabase);
@@ -58,44 +82,26 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-
-// a post route to create new shortened urls
-app.post("/urls", (req, res) => {
-  const userID = req.session.user_id;
-  if (!userID) {
-    return res.status(400).send("<h1>400 Bad Request</h1><p>You need to be logged in in order to short an URL.</p>");
-  }
-  const shortURL = generateRandomString();
-  let longURl = req.body.longURL;
-  if (!(longURl.match(/^(https:\/\/|http:\/\/)/))) {
-    longURl = `http://www.${longURl}`;
-  }
-  urlDatabase[shortURL] = { "longURL": longURl, userID };
-  return res.redirect(`/urls/${shortURL}`);
-});
-
+// check if a corresponding longURL exists and redirect the user accordingly
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL] ? urlDatabase[req.params.shortURL].longURL : "";
-  console.log(longURL);
   if (longURL) {
     return res.redirect(longURL);
   }
   return res.status(400).send("<h1>400 Bad Request</h1><p>The link you are trying to access is not available. It may have been removed from our database by its owner.</p>");
 });
 
-// Add a POST route that removes a URL resource: POST /urls/:shortURL/delete, and redirects the client back to the urls_index page ("/urls").
+// removes an specific url from the urlDatabase
 app.post('/urls/:shortURL/delete', (req, res) => {
   const userID = req.session.user_id;
+  // if no user is logged in, send an error
   if (!userID) {
     return res.status(400).send("<h1>400 Bad Request</h1><p>You need to be logged in in order to delete your URL.</p>");
   }
   const user = users[userID];
   const urls = userURLs(user, urlDatabase);
   const shortURL = req.params.shortURL;
+  // if the user owns the url, delete it as request. Otherwise, inform that the action is not allowed
   if (Object.keys(urls).includes(shortURL)) {
     delete urlDatabase[shortURL];
   } else {
@@ -104,10 +110,10 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect("/urls");
 });
 
-// Add a POST route that updates a URL resource: POST /urls/:shortURL, and redirects the client back to the urls_show page ("/urls/:shortURL").
+// updates an URL and redirects the client back to the urls_show page.
 app.post('/urls/:shortURL', (req, res) => {
-
   const userID = req.session.user_id;
+  // if no user is logged in, send an error
   if (!userID) {
     return res.status(400).send("<h1>400 Bad Request</h1><p>You need to be logged in in order to update your URL.</p>");
   }
@@ -116,6 +122,7 @@ app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURl = req.body.longURL;
 
+  // if the user owns the url, edit as requested (add a protocol if necessary). Otherwise, inform that the action is not allowed
   if (Object.keys(urls).includes(shortURL)) {
     if (longURl.match(/^(https:\/\/|http:\/\/)/)) {
       urlDatabase[shortURL].longURL = longURl;
@@ -128,21 +135,19 @@ app.post('/urls/:shortURL', (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-// Add a GET route to render the register template
+// renders the register template if no user is logged in. Otherwise, redirect to the urls list
 app.get('/register', (req, res) => {
   const userID = req.session.user_id;
   if (userID) {
     return res.redirect('/urls');
   }
-  
-  // SHOULD I PASS THE USER_ID or USER OBJECT???? IF I AM REGISTERING SOMEONE NEW, IT SHOULD MEAN THAT I DON'T HAVE THE COOKIE SET
   const templateVars = {
     user: users[req.session.user_id]
   };
   res.render('register', templateVars);
 });
 
-// Add a POST route to include the new user in our users object if the required fields are filled and the email is not already registered in our users database
+// adds a new user to our database if the required fields are filled and the email is not already registered in our users database
 app.post('/register', (req, res) => {
   const { email, password } = req.body;
   // before accepting the registration, we check if either of the fields are blank or if the email is already registered
@@ -152,8 +157,7 @@ app.post('/register', (req, res) => {
   if (getUserByEmail(email, users)) {
     return res.status(400).send("<h1>400 Bad Request</h1><p>The email is already registered.</p>");
   }
-
-  // if it passes our tests, now we will set the new user;
+  // if it passes our tests, we will set the new user
   const id = generateRandomString();
   users[id] = {
     id,
@@ -164,9 +168,8 @@ app.post('/register', (req, res) => {
   res.redirect('/urls');
 });
 
-// Add a GET route to render the login template
+// renders the login template
 app.get('/login', (req, res) => {
-  // SHOULD I PASS THE USER_ID or USER OBJECT???? IF I HAVE THE COOKIE SET, WHAT SHOULD BE THE EXPECTED BEHAVIOR?
   const userID = req.session.user_id;
   if (userID) {
     return res.redirect('/urls');
@@ -177,19 +180,18 @@ app.get('/login', (req, res) => {
   res.render('login', templateVars);
 });
 
-// Add a route to handle a POST to /login
+// logs a user in if the credentials are valid
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
-  // first check if the login attempt was valid (no blank fields)
+  // checks if the attempt was valid (no blank fields)
   if (email === "" || password === "") {
     return res.status(400).send("<h1>400 Bad Request</h1><p>Please make sure you have filled both the email and password fields.</p>");
   }
-
-  // check if the credentials match with one of our registered users
+  // checks if the credentials match one of our registered users
   const user = getUserByEmail(email, users);
   if (user) {
     if (user.email === email && bcrypt.compareSync(password, user.password)) {
-      // if the credentials given are valid and match our database, a user_id cookie will be set
+      // if all conditions are met, a user_id cookie will be set
       req.session.user_id = user.id;
       return res.redirect('/urls');
     }
@@ -197,7 +199,7 @@ app.post('/login', (req, res) => {
   return res.status(403).send("<h1>403 Forbidden client error </h1><p>Please make sure you are using valid credentials.</p>");
 });
 
-// Add a route to handle a POST to /logout. It clears the user_id cookie and redirects to /urls
+// clears the session cookies and redirects to /urls
 app.post('/logout', (req, res) => {
   res.clearCookie("session");
   res.clearCookie("session.sig");
